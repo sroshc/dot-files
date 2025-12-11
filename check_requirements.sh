2e40516a6030947365d27491d7d0ccce2a418788
Sroshsafa Shirzay <shirzaysroshsafa@gmail.com>
2025-12-10 20:32:15 -0500

    structure files better and fix script

--- check_requirements.sh
+++ check_requirements.sh
@@ -12,18 +12,21 @@ done < requirements.txt
 if [ -f /etc/os-release ]; then
 	. /etc/os-release
 
-	if [[ "$NAME" = "Arch Linux" && "$has_all" = false ]]; then
-		echo "Get to pacmanning."
-	elif [[ "$NAME" = "Gentoo" && "$has_all" = false ]]; then
-		echo "Get to emerging."
-  elif [[ "$NAME" = "Debian GNU/Linux" && "$has_all" = false ]]; then
-    echo "Get to apting."
-  elif [ "$has_all" = true ]; then
-		echo "You have all the reqs!"
-  else
-    echo "Use a better distro."
-  fi
+  if [[ "$has_all" = false ]]; then
 
+    if [[ "$NAME" = "Arch Linux" ]]; then
+      echo "Get to pacmanning."
+    elif [[ "$NAME" = "Gentoo" ]]; then
+      echo "Get to emerging."
+    elif [[ "$NAME" = "Debian GNU/Linux" ]]; then
+      echo "Get to apting."
+    else
+      echo "Use a better distro."
+    fi
+      
+  else
+    echo "You have all the reqs!"
+  fi
 	
 
 fi
