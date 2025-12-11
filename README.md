3301f484cf91bc16048dd13043cd6eaecbd5a227
Sroshsafa Shirzay <shirzaysroshsafa@gmail.com>
2025-12-10 21:37:28 -0500

    added install script and updated readme

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # dot-files
 
-My dot-files for sway. You paid for the whole monitor, you do not need border padding.
+My dot-files for my sway setup alongside some other utilities like mpv, kitty, fastfetch. etc. 
+
 
 Requirements are in `requirements.txt`, `check_requirements.sh` should check if you have all them.
+
+
+`install.sh` should install all the configs and ask you if you want to replace anything if a configuration is present.
+
+
+![beautiful desktop](./pic.png) 
+
+
+> Border padding was invented by monitor companies to sell more monitors
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+  cat <<EOF
+Usage:
+  Installs all configurations in ./config
+    ./install.sh
+
+  Doesn't ask to install configurations if already present:
+    ./install.sh -f
+    ./install.sh --force
+
+  Displays this:
+    ./install.sh -h
+    ./install.sh --help
+EOF
+  exit 0
+fi
+
+seperator="------------------------"
+
+for config in config/*; do
+  if [ -d "$config" ]; then
+    echo "$seperator"
+
+    base=$(basename $config)
+    install_path="$HOME/.config/$base"
+
+    if [[ -d "$install_path" && $1 != "--force"  || $1 != "-f" ]]; then
+      echo "$base config already installed!"
+      read -e -p "Replace it? (y/n): " choice
+      [[ "$choice" == [Yy]* ]] || continue
+    fi
+
+    [ -d $install_path ] && rm -rf "$install_path"
+    cp -f -r "$config" "$install_path"
+    echo "Installed $base $install_path"
+
+  fi
+done
+
+echo "$seperator\n Finished!"
